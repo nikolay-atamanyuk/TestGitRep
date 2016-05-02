e509fdeb2241069cb5fc8b65db27d67e4b562000
Nikolay Atamanyuk <ezhi.mail@gmail.com>
2016-05-02 14:41:44 +0300

    VersionHelper created

--- WorkProduct/WorkProduct/Program.cs
+++ WorkProduct/WorkProduct/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WorkProduct
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("This is version 1.0");
         }
+
     }
 }
---
+++ WorkProduct/WorkProduct/VersionHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkProduct
+{
+    public class VersionHelper
+    {
+        private static readonly int _major = 1;
+        private static readonly int _minor = 0;
+
+        public static string CurrentVersion()
+        {
+            return $"{_major}.{_minor}";
+        }
+    }
+}
