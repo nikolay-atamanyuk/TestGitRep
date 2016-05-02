57ff6c76041f6b1f8bba5707c84f5181fbdaead3
Nikolay Atamanyuk <ezhi.mail@gmail.com>
2016-05-02 14:29:44 +0300

    First release

---
+++ WorkProduct/WorkProduct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is version 1.0");
+        }
+    }
+}
